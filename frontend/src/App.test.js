8de8aec9fe8f33925ca6492aa5453fff56095d5e
pPatr0 <patsev01@easv365.dk>
2026-04-02 20:19:15 +0200

    add frontend react test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders To-Do App heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/to-do app/i);
+  expect(heading).toBeInTheDocument();
 });
